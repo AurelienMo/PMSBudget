1b7bde86c46283ac6211ff7631341b6ccd9aa83c
Aurelien Morvan <morvan.aurelien@gmail.com>
2019-04-19 16:29:18 +0200

    [Components] Custom button || WIP

---
+++ src/views/components/Buttons/MainButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+class MainButton extends React.Component {
+    render() {
+        return undefined;
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
